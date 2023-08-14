8389a37a146ac5950b8b719895f62a0549754ffb
Elyas <Elyas.shamal2@gmail.com>
2023-08-14 15:41:23 -0400

    fetch the data from api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 function Header() {
   return (
     <div className="header">
-      <h1 className="title">Github Users</h1>
+      <h1 className="title">
+        Github Users
+        <span>
+          <FaGithub className="github-icon" />
+        </span>
+      </h1>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,21 @@
-import Header from "./Header";
+import { useEffect, useState } from "react";
 
 // Api link
 const url = "https://api.github.com/users";
 
 function Main() {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setUsers(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
   return <div></div>;
 }
 
